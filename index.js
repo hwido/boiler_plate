ab1967aa66fe87e39db149c64a09b344051a14ec
hwido <hyudojung@naver.com>
2021-09-05 15:00:59 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Hwido:cjswosla99!@boilerplate.zgizs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch((err) => console.log('MongoDB error: ', err));
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listening on port ${port}!')
-)
+
+
+app.get('/', (req, res) => res.send('Hello World!~~안녕하세요!! 즐거운 추석 되세요'))
+
+
+app.post('/register', (req, res) => {
+
+    //회원가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
